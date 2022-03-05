37fe1312c0487ecaa2398ea18b8237993a4f1b4c
Koceww <62842615+KristiyanKoceww@users.noreply.github.com>
2022-03-05 18:06:36 +0200

    Delete Server/Services/MovieLibrary.Services.Mapping directory

--- Server/Services/MovieLibrary.Services.Mapping/IHaveCustomMappings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieLibrary.Services.Mapping
-{
-    using AutoMapper;
-
-    public interface IHaveCustomMappings
-    {
-        void CreateMappings(IProfileExpression configuration);
-    }
-}
